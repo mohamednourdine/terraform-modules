022a803cea33a23a69053808d158bdb90e311561
Mohamed Nourdine <mohamednjikam25@hotmail.com>
2026-04-16 13:16:10 +0200

    Updated the eg and ig settings

--- modules/web-app/main.tf
+++ modules/web-app/main.tf
@@ -39,6 +39,20 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = local.common_tags
 }
 
